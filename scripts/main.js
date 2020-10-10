f3b9f264bf380abb9c28a4dd8c733eb2b33d2e3c
jongster3908 <50344489+jongster3908@users.noreply.github.com>
2020-10-09 18:55:46 -0700

    chapter12

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@
     var Truck = App.Truck;
     var DataStore = App.DataStore;
     var FormHandler = App.FormHandler;
+    var Validation = App.Validation;
     var CheckList = App.CheckList;
     var myTruck = new Truck('ncc-1701', new DataStore());
     window.myTruck = myTruck;
@@ -17,4 +18,7 @@
         myTruck.createOrder.call(myTruck, data);
         checkList.addRow.call(checkList, data);
     });
+
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+    
 }) (window);
