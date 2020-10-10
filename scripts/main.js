d0e88b395efd63e9e0ecdc12950c404071fb798e
jongster3908 <50344489+jongster3908@users.noreply.github.com>
2020-10-09 19:57:00 -0700

    added eslint

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
     'use strict';
     var FORM_SELECTOR = '[data-coffee-order="form"]';
     var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
-    //  var SERVER_URL = 'https://co.audstanley.com/coffeeorders';    // if running on the shared server
+    // var SERVER_URL = 'https://co.audstanley.com/coffeeorders';    // if running on the shared server
     var SERVER_URL = 'http://localhost:3000/coffeeorders';          // if running locally
     var App = window.App;
     var Truck = App.Truck;
--- scripts/validation.js
+++ scripts/validation.js
@@ -3,9 +3,19 @@
     var App = window.App || {};
 
     var Validation = {
-        isCompanyEmail: function (email) {
-            return /.+@bignerdranch\.com$/.test(email);
+
+        isCompanyEmail: function(email) {
+
+            return /.+@starfleet\.com$/.test(email);
+
+        },
+
+        isHighCaffeine: function(caffeine) {
+
+            return caffeine > 50;
+
         }
+
     };
 
     App.Validation = Validation;
