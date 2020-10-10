6e18f0b4a64f916832702f06e368d87439c42c70
jongster3908 <50344489+jongster3908@users.noreply.github.com>
2020-10-09 19:16:41 -0700

    chapter13

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,17 @@
     'use strict';
     var FORM_SELECTOR = '[data-coffee-order="form"]';
     var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+    //  var SERVER_URL = 'https://co.audstanley.com/coffeeorders';    // if running on the shared server
+    var SERVER_URL = 'http://localhost:3000/coffeeorders';          // if running locally
     var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
+    var RemoteDataStore = App.RemoteDataStore;
     var FormHandler = App.FormHandler;
     var Validation = App.Validation;
     var CheckList = App.CheckList;
-    var myTruck = new Truck('ncc-1701', new DataStore());
+    var remoteDS = new RemoteDataStore(SERVER_URL);
+    var myTruck = new Truck('ncc-1701', remoteDS);
     window.myTruck = myTruck;
     var checkList = new CheckList(CHECKLIST_SELECTOR);
     checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
@@ -20,5 +24,5 @@
     });
 
     formHandler.addInputHandler(Validation.isCompanyEmail);
-    
+
 }) (window);
